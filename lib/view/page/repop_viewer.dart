5cbed74b0d5b024a8325831febb613549e3856ee
yuki_kamikita <akaiyukiusagi@gmail.com>
2021-11-14 20:18:42 +0900

    ディレクトリ変更時の修正漏れ

--- lib/view/page/repop_viewer.dart
+++ lib/view/page/repop_viewer.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
-import '../../SharedPreference/preference_key.dart';
+import '../../model/sharedPreference/preference_key.dart';
 import '../widget/accordion.dart';
 import '../widget/drawable_text.dart';
 
